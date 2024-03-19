fcc0fa78ea17ca0caef465796ed2e70342a2dccf
Yashika Agrawal <97408966+Yashika-Agrawal@users.noreply.github.com>
2024-03-20 02:31:34 +0530

    Printing, taking input and variables

---
+++ Day 1/main.py
@@ -0,0 +1,12 @@
+# Printing statements
+print("Hello world\nHello Angela") # \n is used to print in next line
+print("Hello " + "Angela") # + is used to concatenate strings
+
+#input() function
+print ("Hello " + input("What's your name?"))
+print(len(input())) #length of characters in input
+
+#variables
+name = input("What's your name?")
+length= len(name)
+print(length)
