693105de125005f61ea4c287fce417cff5e7ab56
Yashika Agrawal <97408966+Yashika-Agrawal@users.noreply.github.com>
2024-03-20 21:51:37 +0530

    Day 2: Data types and operations

---
+++ Day 2/main.py
@@ -0,0 +1,53 @@
+#Data Types
+#String
+
+print("Hello"[0])
+print("Hello"[4])
+
+#Integer
+print(123 + 345)
+print(123_456_789) #_ can be written to make it more readable
+
+#Float
+3.14159
+
+#Boolean
+True
+False
+
+#operations
+3 + 5
+4 - 3
+4 * 3
+6 / 3 #this will give float number in the output - 2.0
+2 ** 3 #exponent
+
+#PEMDAS
+# Parentheses ()
+# Exponents **
+# Multiplication *
+# Division /
+# Addition +
+# Subtraction -
+
+print ( 3 * (3 + 3) /3 - 3) #3
+
+#rounding
+print(round(8/3, 2)) #2.67
+print(round(2.66666666, 2)) #2.67
+
+#floor division
+print(8//3) #2
+
+#shorthand property for operations
+score=0
+score+=1
+h = 4.56
+isWinning = True
+#f-string
+print(f"your score is {score}, your height is {h}, you are winning is {isWinning} ")
+
+
+
+
+
